6df2b0144914e0601343f4e6d3654cd78dde0f5b
DELL <DELL@Dell3511-win11off19>
2025-03-03 13:01:32 +0630

    beginner

---
+++ exception-project/src/test/java/com/jdc/grace/test/HandlerTest.java
@@ -0,0 +1,18 @@
+package com.jdc.grace.test;
+
+import org.junit.jupiter.api.Test;
+
+import com.jdc.grace.handler.Handler;
+
+public class HandlerTest {
+	
+
+		@Test
+		void testName() {
+			Handler handle = new Handler();
+			handle.useName();
+		}
+	}
+			
+			
+	
