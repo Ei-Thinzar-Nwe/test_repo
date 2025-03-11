bde21bc8fafd9ff90bb34af8d5581cbc06831363
DELL <DELL@Dell3511-win11off19>
2025-03-11 14:45:57 +0630

    Beginning

--- 02-java5-annotation/src/main/java/com/jdc/grace/GradeAdder.java
+++ 02-java5-annotation/src/main/java/com/jdc/grace/GradeAdder.java
@@ -5,7 +5,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import com.jdc.grace.Grade;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
